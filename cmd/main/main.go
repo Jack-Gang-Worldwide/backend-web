f770d7e746a79602b37d6c7692928091bb847e27
Maheswara Devo <pundadevo21@gmail.com>
2022-03-22 15:42:46 +0800

    add mysql driver

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/Jack-Gang-Worldwide/backend-web/pkg/controller"
 	"github.com/SIC-Unud/sicgolib"
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 	"github.com/joho/godotenv"
 )
