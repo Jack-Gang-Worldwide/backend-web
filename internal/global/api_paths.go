764baf55229e633ed5bb8031580b60da9015c33a
Maheswara Devo <pundadevo21@gmail.com>
2022-03-25 16:20:52 +0800

    [#7] Add view mobile legends members

--- internal/global/api_paths.go
+++ internal/global/api_paths.go
@@ -1,7 +1,8 @@
 package global
 
 const (
-	API_PATH_GET_ALL_MEMBERS            = "/members"
-	API_PATH_GET_ALL_VALORANT_MEMBERS   = "/valorant/members"
-	API_PATH_GET_ALL_THE_FOREST_MEMBERS = "/theforest/members"
+	API_PATH_GET_ALL_MEMBERS                = "/members"
+	API_PATH_GET_ALL_VALORANT_MEMBERS       = "/valorant/members"
+	API_PATH_GET_ALL_THE_FOREST_MEMBERS     = "/theforest/members"
+	API_PATH_GET_ALL_MOBILE_LEGENDS_MEMBERS = "/mobilelegends/members"
 )
--- internal/mobilelegends/service/api/mobilelegends_service_api.go
+++ internal/mobilelegends/service/api/mobilelegends_service_api.go
@@ -1,4 +1,11 @@
 package api
 
+import (
+	"context"
+
+	"github.com/Jack-Gang-Worldwide/backend-web/internal/mobilelegends/dto"
+)
+
 type MobileLegendsService interface {
+	GetAllMobileLegendsMembers(ctx context.Context) (dto.MembersResponse, error)
 }
