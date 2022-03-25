043fac84a62cf7f30c4d9931d9889618aa0b47cc
Maheswara Devo <pundadevo21@gmail.com>
2022-03-26 00:50:10 +0800

    add post path routes

--- internal/global/api_paths.go
+++ internal/global/api_paths.go
@@ -5,4 +5,5 @@ const (
 	API_PATH_GET_ALL_VALORANT_MEMBERS       = "/valorant/members"
 	API_PATH_GET_ALL_THE_FOREST_MEMBERS     = "/theforest/members"
 	API_PATH_GET_ALL_MOBILE_LEGENDS_MEMBERS = "/mobilelegends/members"
+	API_PATH_INSERT_MEMBERS                 = "/members"
 )
